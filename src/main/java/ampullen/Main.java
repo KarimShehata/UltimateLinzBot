3487b8b47447ec0ca0ab50ef1eb3482e6d7de07d
Unknown <karim.shehata1@gmail.com>
2018-10-14 23:07:44 +0200

    Implemented reading discord bot token from .txt file

--- src/main/java/ampullen/Main.java
+++ src/main/java/ampullen/Main.java
@@ -1,12 +1,8 @@
 package ampullen;
 
-import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.*;
 
 import javax.security.auth.login.LoginException;
-
-import org.json.JSONTokener;
-import org.json.JSONWriter;
 
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -18,19 +14,26 @@ import net.dv8tion.jda.core.hooks.EventListener;
 public class Main{
 
 	public static void main(String[] args){
-		
-		JDA jda = null;
+
+        String token = GetToken();
+        
+        if(token.isEmpty()) {
+            System.out.println("token.txt is empty");
+            System.exit(0);
+        }
+
+        JDA jda = null;
 		try {
-			jda = new JDABuilder("NDk4OTQ5MTg4MzQ4OTM2MTky.Dp3YEg.jjeXldfvrQncYJZDwC3Sl_o8QBE")
+			jda = new JDABuilder(token)
 					.setGame(Game.playing("Ultimate Frisbee"))
 					.build();
 		} catch (LoginException e) {
 			e.printStackTrace();
 		}
-		
+
 		//JDABot bot = jda.asBot();
 		jda.addEventListener(new EventListener() {
-			
+
 			public void onEvent(Event event) {
 				if(event instanceof ReadyEvent) {
 					System.out.println("API is ready");
@@ -39,8 +42,37 @@ public class Main{
 		});
 		jda.addEventListener(new MainListener());
 		jda.addEventListener(new TournamentListener());
-		
+
 		
 	}
-	
+
+	//reads the discord bot token from token.txt
+    private static String GetToken() {
+
+        String token = "";
+
+        File file = new File("token.txt");
+
+        if (!file.exists()) {
+            System.out.println("token.txt file not found.");
+            System.exit(0);
+        }
+        
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+                line = bufferedReader.readLine();
+            }
+            token = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return token;
+    }
+
 }
