c970686b2221ccf46298684a8bb0b45e08c783ee
rpanic <raphael.panic@gmx.at>
2018-11-01 01:05:43 +0100

    Finished Conversion Class
    See Example at TournamentListener on how to use it

--- src/main/java/ampullen/Conversation.java
+++ src/main/java/ampullen/Conversation.java
@@ -1,9 +1,11 @@
 package ampullen;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
@@ -69,13 +71,30 @@ public class Conversation implements Cloneable{
 		public Option addOption(String question, BiFunction<String, Option, Object> function) {
 			
 			Option o = new Option(c, this);
-			o.addStep(question, function);
+			
+			String questionstr = null;
+			
+			if(question.contains("-")){
+				String[] arr = question.split("-");
+				questions.put(arr[0], arr[1]);
+				questionstr = arr[1];
+			}else{
+				questionstr = question;
+			}
+			
+			o.addStep(questionstr, function);
+			order.add(o);
+			//map.put(question, (x, y) -> x);
 			
 			return o;
 		}
 		
 		public Option addOption(String question, Function<String, Object> function) {
 			return addOption(question, (x, y) -> function.apply(x));
+		}
+		
+		public Option addOption(String id, String question, BiFunction<String, Option, Object> function){
+			return addOption(id + "-" + question, function);
 		}
 		
 		public Option addStep(String question, BiFunction<String, Option, Object> function){
@@ -117,7 +136,7 @@ public class Conversation implements Cloneable{
 			//String last = this.last;
 			return addStep(question, (x) -> {
 				function.accept(x); 
-				return getNext();
+				return getNext(null);
 			});
 		}
 		
@@ -125,22 +144,40 @@ public class Conversation implements Cloneable{
 			//String last = this.last;
 			return addStep(question, (x, y) -> {
 				function.accept(x, y); 
-				return getNext();
+				return getNext(null);
 			});
 		}
 		
-		String last;
+		List<String> last = new ArrayList<>();
 		
 		public Option addStepLast(String question, BiConsumer<String, Option> function) {
 			addStepA(question, function);
-			last = question;
+			last.add(question);
 			return this;
 		}
 		
-		public Object getNext() {
-			int index = order.indexOf(current);
-			if(index < order.size()-1) {
-				return order.get(index + 1);
+		public Option addStepLast(String id, String question, BiConsumer<String, Option> function){
+			return addStepLast(id + "-" + question, function);
+		}
+		
+		public Object getNext(Object s) {
+			if(s == null || !(s instanceof String)){
+				int index = order.indexOf(current);
+				if(index < order.size()-1) {
+					return order.get(index + 1);
+				}
+			}else{
+				return order.stream().filter(x -> {
+					if(x instanceof String){
+						return x.equals(s);
+					}else if(x instanceof Option){
+						Option o = (Option)x;
+						Object s2 = o.order.get(0);
+						Entry<String, String> entry = o.questions.entrySet().stream().filter(y -> y.getKey().equals(s)).findFirst().orElse(null);
+						return (entry != null ? true : false) || s2.equals(s);
+					}
+					return false;
+				}).findFirst().orElse(null);
 			}
 			return null;
 		}
@@ -159,6 +196,7 @@ public class Conversation implements Cloneable{
 		}
 		
 		protected boolean completed = false;
+		protected String lastAnswer = null;
 		
 		public Option start(MessageChannel channel, User user) {
 			
@@ -172,34 +210,50 @@ public class Conversation implements Cloneable{
 				completed = true;
 			};
 			
-			if(parent != null) {
+			if(parent == null) {
 				t = new Thread(run);
 				t.start();
 			}else {
 				run.run();
 			}
-			
 			return this;
 		}
-
+		
 		private void askNext(MessageChannel channel, User user) {
 			
 			String answer = null;
 			
 			if(current instanceof String) {
 				
-				answer = new Prompt((String)current, channel, user).promptSync();
+				if(last.stream().map(x -> x.split("-")).flatMap(x -> Arrays.stream(x)).anyMatch(x -> x.equals(current))){
+					channel.sendMessage(current.toString()).complete();
+					lastAnswer = null;
+				}else{
+					lastAnswer = answer = new Prompt((String)current, channel, user).promptSync();
+				}
 				
 			}else if(current instanceof Option) {
 				
 				Option o = (Option)current;
 				o.start(channel, user);
-				answer = o.completed + "";
+				//answer = o.completed + "";
+				lastAnswer = answer = o.lastAnswer;
 				
 			}
-			map.get(current).apply(answer, this);
-			current = getNext();
-			
+			if(current.equals("Was dann?")){
+				System.out.println();
+			}
+			if(answer != null){
+				Object o = map.get(current).apply(answer, this);
+				if(o instanceof String){
+					if(questions.containsKey((String)o)){
+						o = questions.get((String)o);
+					}
+				}
+				
+				current = getNext(o);			
+			}else
+				current = null;
 		}
 		
 	}
