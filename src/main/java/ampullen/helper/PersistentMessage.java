282bab1f37a436725edbd00084bb48e3f6d8a46a
rpanic <raphael.panic@gmx.at>
2018-11-14 11:09:25 +0100

    Added PersistentMessage Class

---
+++ src/main/java/ampullen/helper/PersistentMessage.java
@@ -0,0 +1,50 @@
+package ampullen.helper;
+
+import java.util.List;
+
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+
+public class PersistentMessage {
+	
+	String messageStr;
+	public EmoteLimiter limiter;
+	MessageChannel c;
+	List<String> emotes;
+	Message message;
+	boolean pinned = false;
+	
+	public PersistentMessage(MessageChannel c, String message, List<String> emotes) {
+		this.messageStr = message;
+		this.c = c;
+		this.emotes = emotes;
+	}
+	
+	public PersistentMessage(MessageChannel c, String message, List<String> emotes, boolean pinned) {
+		this(c, message, emotes);
+		this.pinned = pinned;
+	}
+	
+	public PersistentMessage(Message m, List<String> emotes, boolean pinned) {
+		this.message = m;
+		this.emotes = emotes;
+		this.pinned = pinned;
+		this.c = m.getChannel();
+	}
+	
+	public void init() {
+		if(message == null) {
+			message = c.sendMessage(message).complete();
+		}
+		limiter = new EmoteLimiter(message)
+				.setAllowedEmotes(emotes)
+				.setDisplayAllowed(true)
+				.setLimitEmotes(true)
+				.setLimitReactions(true);
+		limiter.start(c);
+		
+		if(pinned)
+			message.pin();
+	}
+	
+}
